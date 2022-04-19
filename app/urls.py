2e063132b4a52dcea7a6381bbaeb41e72c7115bf
Rupakkk <rupak.fcc@gmail.com>
2022-04-19 21:37:01 +0545

    third

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from app import views
 urlpatterns = [
+    path('',views.IndexView.as_view(),name='index'),
     path('',views.HomeView.as_view(),name='home'),
-    path('post/',views.PostView.as_view(),name="post")
+    path('profile/',views.PostView.as_view(),name="post")
 ]
--- app/views.py
+++ app/views.py
@@ -1,19 +1,26 @@
 
+from re import template
 from django.shortcuts import render
-from django.views.generic import CreateView,ListView
+from django.views.generic import CreateView,ListView,TemplateView
 from app.models import FormModel
 
 
 # Create your views here.
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+
+
+
 class HomeView(CreateView):
     model = FormModel
     success_url = 'post'
     fields = '__all__'
-    template_name = 'index.html'
+    template_name = 'accounts/login.html'
    
 class PostView(ListView):
     model = FormModel
-    template_name = 'post.html'
+    template_name = 'registration/profile.html'
     context_object_name = 'forms'
     
 
